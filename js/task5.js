a9a93955447f627fbecc3eec759c71fa1fc7dc80
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-01-03 11:22:51 +0100

    d

--- js/task5.js
+++ js/task5.js
@@ -7,41 +7,116 @@
 //быть в произвольном регистре.
 
 
-const checkForSpam = function(message) {
-  // твой код
- const SPAM = 'spam'
-  const SALE = 'sale'
+// const checkForSpam = function(message) {
+//   // твой код
+//  const SPAM = 'spam'
+//   const SALE = 'sale'
 
 
-  const wordToLowerCase = message.toLowerCase();
-  let wordSplit = wordToLowerCase.split(' ');
- console.log(wordSplit)
-  // for (let i = 0; i < message.length; i += 1){
+//   const wordToLowerCase = message.toLowerCase();
+//   let wordSplit = wordToLowerCase.split(' ');
+//  console.log(wordSplit)
+//   // for (let i = 0; i < message.length; i += 1){
  
-//     let word =wordSplit[i]
-//          console.log(String(word))
-//     if (word === SALE) {
-//       return true
-//     } else if (word === SPAM) {
-//       return true
+// //     let word =wordSplit[i]
+// //          console.log(String(word))
+// //     if (word === SALE) {
+// //       return true
+// //     } else if (word === SPAM) {
+// //       return true
+// //     }
+// //   }
+// //  return false
+// };
+//  const SPAM = 'spam'
+//   const SALE = 'sale'
+//   let result;
+
+//   const wordToLowerCase = message.toLowerCase();
+//   let result = wordToLowerCase.includes(SPAM)||wordToLowerCase.includes(SALE);
+//  return result
+// /*
+//   Вызовы функции для проверки работоспособности твоей реализации.
+//  */
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+// const clients = ['Mango', 'Ajax', 'Poly'];
+
+// for (const client of clients) {
+//   console.log(client)
+// }
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Пиши код ниже этой строки
+
+//   for (const number of order) {
+//    total += number
+//     console.log(total)
+//   }
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4]))
+
+// function filterArray(numbers, value) {
+//   // Пиши код ниже этой строки
+//   const filteredNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number > value) {
+//           filteredNumbers.push(number);
+//   }
+// }
+
+
+//   // for (let i = 0; i < numbers.length; i += 1) {
+//   //   const number = numbers[i];
+
+//   //   if (number > value) {
+//   //     filteredNumbers.push(number);
+//   //   }
+//   // }
+
+//   return filteredNumbers;
+//   // Пиши код выше этой строки
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3) )
+
+// function getEvenNumbers(start, end) {
+//     // Пиши код ниже этой строки
+//   let newArrow = []
+//   for (let i = start; i <= end; i += 1){
+//     console.log(i)
+//     if (i % 2 === 0) {
+//       newArrow.push(i)
 //     }
 //   }
-//  return false
-};
- const SPAM = 'spam'
-  const SALE = 'sale'
-  let result;
 
-  const wordToLowerCase = message.toLowerCase();
-  let result = wordToLowerCase.includes(SPAM)||wordToLowerCase.includes(SALE);
- return result
-/*
-  Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(checkForSpam('Latest technology news')); // false
+// return newArrow
+// }
+//   console.log(getEvenNumbers(3, 11))
 
-console.log(checkForSpam('JavaScript weekly newsletter')); // false
+function includes(array, value) {
+  // Пиши код ниже этой строки
 
-console.log(checkForSpam('Get best sale offers now!')); // true
+  for (let number of array) {
+    console.log(number);
+    if (number === value) {
+      return true
+    }
+  }
+    //   }else{
+    //     return false}
+      
+    // }
+  // Пиши код выше этой строки
+}
 
-console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+console.log(includes([1, 2, 3, 4, 5], 3));
